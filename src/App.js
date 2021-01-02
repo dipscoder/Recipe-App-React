ffdd837e6756e29e6c6a005ecd712ec0122ced15
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-02 17:26:33 +0530

    Fetch data from the api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
   const APP_ID = "364d5953";
   const APP_KEY = "8200aaf03f1b1277860d6080dd883053";
-  const exampleReq =
-    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getRecipes = async () => {
+    const response = await fetch(exampleReq)
+    const data = await response.json()
+    console.log(data);
+  }  
+  
+  useEffect(() => {
+    getRecipes()
+  }, [])
+
+
 
   return ( 
     <div className="App">
