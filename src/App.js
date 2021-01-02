09d7959e6987ec7661dd46bed8e8f54fe61b9ee0
dipscoder <dipeshjaiswal140@gmail.com>
2021-01-02 17:19:47 +0530

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
+  const APP_ID = "364d5953";
+  const APP_KEY = "8200aaf03f1b1277860d6080dd883053";
+  const exampleReq =
+    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-button">Search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
